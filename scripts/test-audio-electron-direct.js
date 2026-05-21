bed995c932317dea4d2f93456dacf54b0bfd7a0a
Edenware <efox.web@gmail.com>
2026-05-21 13:52:46 -0300

    Move test_audio_load_electron_direct.js into scripts and archive start.cmd, icon-extractor.ps1, partner-center.js

--- scripts/test-audio-electron-direct.js
+++ scripts/test-audio-electron-direct.js
@@ -2,7 +2,7 @@ const { spawnSync } = require('child_process');
 const path = require('path');
 const electronExe = require('electron');
 
-const testFile = path.resolve(__dirname, '..', 'test_audio_load_electron_direct.js');
+const testFile = path.resolve(__dirname, 'test_audio_load_electron_direct.js');
 const result = spawnSync(electronExe, [testFile], {
   stdio: 'inherit',
   windowsHide: false
